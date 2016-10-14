1216e0498390124ae7e83dc465ba87859e924ef7
Tomas <shostakas@gmail.com>
2016-10-14 16:15:44 +0200

    Checkboxes

--- carrefour-app/src/pages/home/home.ts
+++ carrefour-app/src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 import { ItemSliding } from 'ionic-angular';
+import { MenuController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -12,14 +13,42 @@ export class HomePage {
   public items = [
     { id : 1, name: "Tomas" },
     { id : 2, name: "Jeroen" },
+    { id : 3, name: "Ryan" },
+    { id : 4, name: "Tomas" },
+    { id : 5, name: "Jeroen" },
+    { id : 6, name: "Ryan" },
+    { id : 7, name: "Tomas" },
+    { id : 8, name: "Jeroen" },
+    { id : 9, name: "Ryan" }
+  ];
+
+  public suggestedItems = [
+    { id : 1, name: "Tomas" },
+    { id : 2, name: "Jeroen" },
     { id : 3, name: "Ryan" }
   ];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public menuCtrl: MenuController) {
   }
 
   share(slidingItem: ItemSliding) {
     slidingItem.close();
   }
+
+  add() {
+    this.items.push({ id : 4, name: "Heya" });
+  }
+
+ openMenu() {
+   this.menuCtrl.open();
+ }
+
+ closeMenu() {
+   this.menuCtrl.close();
+ }
+
+ toggleMenu() {
+   this.menuCtrl.toggle();
+ }
 
 }
