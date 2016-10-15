f5ea39c26a9e6710ce8fccfab9b97a86b1e1b7ec
uodasuodas <somiciuss@gmail.com>
2016-10-15 20:28:13 +0300

    pushy

--- carrefour-app/src/pages/home/home.ts
+++ carrefour-app/src/pages/home/home.ts
@@ -3,52 +3,168 @@ import { Component } from '@angular/core';
 import { NavController, ToastController } from 'ionic-angular';
 import { ItemSliding } from 'ionic-angular';
 
+Array.prototype.diff = function(a) {
+  return this.filter(function(i) {return a.indexOf(i) < 0;});
+};
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
+
 export class HomePage {
 
+  constructor(public navCtrl: NavController, private toastCtrl: ToastController) {
+    this.recalculate();
+  }
+
+  // DEFINITIONS
+
   public myItems = [
-    { id : 1, name: "Tomas", price: 5 },
-    { id : 2, name: "Jeroen", price: 5 },
-    { id : 3, name: "Ryan", price: 5 },
-    { id : 4, name: "Tomas", price: 5 },
-    { id : 5, name: "Jeroen", price: 5 }
+    { id : 1, name: "Milk", discounted: 0 },
+    { id : 2, name: "Cookies", discounted: 0 },
+    { id : 3, name: "Beer", discounted: 0 },
+    { id : 4, name: "Diapers", discounted: 0 },
+    { id : 5, name: "Nuts", discounted: 0 }
   ];
 
   public suggestedItems = [
-    { id : 1, name: "Tomas", price: 5 },
-    { id : 2, name: "Jeroen", price: 5 },
-    { id : 3, name: "Ryan", price: 'free' }
+    { id : 1, name: "Snickers", discounted: 0 },
+    { id : 2, name: "Fish crackers", discounted: 0 },
+    { id : 3, name: "Corn Syrup", discounted: 0 }
   ];
 
   public promotions = [
     {
-      items: ['','',''],
-      discount: 5,
-      freeItem: ''
+      id: 1,
+      items: ['Milk','Cookies','Corn Syrup'],
+      discountType: 2,
+      discountAmount: 5,
+      discountedItem: {
+        id : 6, name: "Free Snickers", discounted: 1
+      }
     },
-  ]
+    {
+      id: 2,
+      items: ['Beer','Diapers','Mars','Condoms'],
+      discountType: 1,
+      discountAmount: 50,
+      discountedItem: {
+        id : 7, name: "Free Fish crackers", discounted: 1
+      }
+    }
+  ];
 
-  public removedItems = [];
+  public categoryColors = {
+    id_1: '#FF0000',
+    id_2: '#800000'
+  };
 
-  constructor(public navCtrl: NavController, private toastCtrl: ToastController) {
-  }
+  // FUNCTIONS
+  recalculate() {
 
-  share(slidingItem: ItemSliding) {
-    slidingItem.close();
-  }
+    // --------- myList generation
+    // Adding discounted items
+    var discounts = this.getFulfilledDiscountInfo(this.myItems);
+    for (var i=0; i < discounts.length; i++) {
+      if (!this.subsetOf([discounts[i].discountedItem.name], this.namesArrayFromObjList(this.myItems))){
+        this.myItems.push(discounts[i].discountedItem);
+      }
+    }
+    // Removing discounted items
+    for(var i=0; i < this.myItems.length; i++) {
+      if (this.myItems[i].discounted == 1) {
+        if (!this.isLegalDiscount(this.myItems[i], discounts)) {
+           this.myItems.splice(i,1);
+        }
+      }
+    }
 
-  add(item) {
-    this.myItems.push(item);
-    for (var i=0; i < this.suggestedItems.length; i++) {
-      if (this.suggestedItems[i].id === item.id) {
-        this.suggestedItems.splice(i,1);
+    // --------- Suggested items generation from missing for discount items
+    var missing = this.getMissingItems(this.myItems);
+    for (var i = 0; i < missing.length; i++) {
+      for (var k = 0; k < missing[i].items.length; k++) {
+        if (!this.subsetOf([missing[i].items[k]], this.namesArrayFromObjList(this.suggestedItems))){
+          var id = Math.round(Math.random()*100) + 1;
+          this.suggestedItems.push({
+            id: id,
+            name: missing[i].items[k],
+            discounted: 0
+          });
+        }
       }
     }
   }
-  presentToast(item) {
+
+  public isLegalDiscount(item, discounts){
+    for(var k=0; k < discounts.length; k++) {
+      if (item.name == discounts[k].discountedItem.name){
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public getCategoryColor(id) {
+    return this.categoryColors['id_' + id];
+  }
+
+  // Given an item object, fins to which discount categories it belongs to
+  public getItemDiscountCategories(item) {
+    var ids = [];
+    for (var i = 0; i < this.promotions.length; i++) {
+      var items = this.promotions[i].items;
+      for (var k = 0; k < items.length; k++) {
+        if (item.name == items[k]) {
+          ids.push(this.promotions[i].id);
+        }
+      }
+    }
+    return ids;
+  }
+
+  public subsetOf(arr1, arr2) {
+    return arr1.every(elem => arr2.indexOf(elem) > -1);
+  }
+
+  public namesArrayFromObjList(objArr){
+    var arr = [];
+    for (var i=0; i < objArr.length; i++) {
+      arr.push(objArr[i].name);
+    }
+    return arr;
+  }
+
+  public getFulfilledDiscountInfo(itemList){
+    var discounts = [];
+    for (var i=0; i < this.promotions.length; i++) {
+      var categoryItems = this.promotions[i].items;
+      var itemNames = this.namesArrayFromObjList(itemList);
+      if (this.subsetOf(categoryItems, itemNames)){
+        discounts.push(this.promotions[i]);
+      }
+    }
+    return discounts;
+  }
+
+  public getMissingItems(itemList){
+    var missing = [];
+    for (var i=0; i < this.promotions.length; i++) {
+      var categoryItems = this.promotions[i].items;
+      if (!this.subsetOf(itemList, categoryItems)) {
+        var itemNames = this.namesArrayFromObjList(itemList);
+        var missingPerCat = categoryItems.diff(itemNames);
+        missing.push({
+          id: this.promotions[i].id,
+          items: missingPerCat
+        });
+      }
+    }
+    return missing;
+  }
+
+  public presentToast(item) {
     let toast = this.toastCtrl.create({
       message: 'Item deleted',
       showCloseButton: true,
@@ -63,21 +179,37 @@ export class HomePage {
     });
   }
 
-  remove(item) {
-    this.suggestedItems.push(item);
+  public add(item) {
+    if (!this.subsetOf([item.name], this.namesArrayFromObjList(this.myItems))){
+      this.myItems.push(item);
+    }
+    for (var i=0; i < this.suggestedItems.length; i++) {
+      if (this.suggestedItems[i].id === item.id) {
+        this.suggestedItems.splice(i,1);
+      }
+    }
+    this.recalculate();
+  }
+
+  public remove(item) {
+    if (!this.subsetOf([item.name], this.namesArrayFromObjList(this.suggestedItems))){
+      this.suggestedItems.push(item);
+    }
     for (var i=0; i < this.myItems.length; i++) {
-      if (this.myItems[i].id === item.id) {
-        //this.removedItems.push(this.myItems[i]);
+      if (this.myItems[i].name === item.name) {
         this.presentToast(item);
         this.myItems.splice(i,1);
       }
     }
+    this.recalculate();
   }
 
-  totalCartPrice() {
+  public totalCartPrice() {
     var price: number = 0;
     for (var i=0; i < this.myItems.length; i++) {
-      price += this.myItems[i].price;
+      if (this.myItems[i].price > 0) {
+        price += this.myItems[i].price;
+      }
     }
     return price;
   }
@@ -92,15 +224,6 @@ export class HomePage {
       plural = 's';
     }
     return word + plural;
-  }
-
-  euro(int) {
-    if (int > 0) {
-      var nr:String = String(int);
-      return nr + '€';
-    } else {
-      return int;
-    }
   }
 
   showConfirm(item) {
